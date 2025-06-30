e2d017a7d26152c249460822950c47bc42c01813
Aryan-Kumar-7 <rajaryankumar26april@gmail.com>
2025-06-30 17:45:29 +0530

    SensAI

---
+++ app/layout.js
@@ -0,0 +1,48 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import Header from "@/components/header";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+
+export const inter = Inter({
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "SensAI - AI Career Coach",
+  description: "SensAI is your AI career coach, helping you navigate your career path with personalized advice and insights.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider appearance={{
+      baseTheme: dark,
+    }}>
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={`${inter.className}`}
+        >
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {/* header */}
+            <Header />
+            <main className="min-h-screen">
+              {children}
+            </main>
+            {/* footer */}
+            <footer className="bg-muted/50 py-12">
+              <div className="container mx-auto px-4 text-center text-gray-200">
+                <p>Made with Raj</p>
+              </div>
+            </footer>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
