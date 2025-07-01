e1e970f58521f6e37ae22aeddd58c22d2ca622be
Aryan-Kumar-7 <rajaryankumar26april@gmail.com>
2025-07-01 16:25:32 +0530

    gemini setup and onboarding

--- app/(main)/onboarding/page.jsx
+++ app/(main)/onboarding/page.jsx
@@ -1,7 +1,20 @@
+import { industries } from "@/data/industries"
+import OnboardingForm from "./_component/onboarding-form"
+import { getUserOnboardingStatus } from "@/actions/user";
+import { redirect } from "next/navigation";
 
-const OnboardingPage = () => {
+
+const OnboardingPage = async () => {
+    // Check if User is onboarded
+    const { isOnboarded } = await getUserOnboardingStatus();
+    if(isOnboarded) {
+        redirect("/dashboard");
+    }
+
   return (
-    <div>OnboardingPage</div>
+    <main>
+        <OnboardingForm industries={industries} />
+    </main>
   )
 }
 
